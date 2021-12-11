3a8e11d86824c6d140e1633e74347456efa6aac2
Victor Simonin <victor.simonin@epita.fr>
2021-12-11 14:43:00 +0100

    2021 day 2 exercice 1

---
+++ 2021/2/2.py
@@ -0,0 +1,31 @@
+def import_data():
+    data = open('2021/2/input.txt', 'r')
+    lines = data.readlines()
+    return lines
+
+############### First Exercise ###############
+
+def get_position(lines):
+
+    horizontal_position = 0
+    depth_position = 0
+
+    for i in range(len(lines)):
+        move = lines[i].split()
+        if move[0][0] == 'f':
+            horizontal_position += int(move[1])
+        elif move[0][0] == 'u':
+            depth_position -= int(move[1])
+        else:
+            depth_position += int(move[1])
+
+    return horizontal_position * depth_position
+
+lines = ["forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"] 
+result = get_position(lines)
+print ("\nThe Result of the first test is :",result)
+
+lines = import_data()
+result = get_position(lines)
+print ("The Result of the first exercise is :",result)
+print()
