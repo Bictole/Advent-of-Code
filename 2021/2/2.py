0892c2d503c64f4c4a78b335f5488b650ecfb656
Victor Simonin <victor.simonin@epita.fr>
2021-12-12 19:22:00 +0100

    2021 day 2 exercise 2

--- 2021/2/2.py
+++ 2021/2/2.py
@@ -29,3 +29,32 @@ lines = import_data()
 result = get_position(lines)
 print ("The Result of the first exercise is :",result)
 print()
+
+############### Second Exercise ###############
+
+def get_aim_position(lines):
+
+    horizontal_position = 0
+    depth_position = 0
+    aim = 0
+
+    for i in range(len(lines)):
+        move = lines[i].split()
+        if move[0][0] == 'f':
+            horizontal_position += int(move[1])
+            depth_position += (aim * int(move[1]))
+        elif move[0][0] == 'u':
+            aim -= int(move[1])
+        else:
+            aim += int(move[1])
+
+    return horizontal_position * depth_position
+
+lines = ["forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"] 
+result = get_aim_position(lines)
+print ("\nThe Result of the second test is :",result)
+
+lines = import_data()
+result = get_aim_position(lines)
+print ("The Result of the second exercise is :",result)
+print()
