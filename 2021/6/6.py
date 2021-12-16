eb37310300eefebfc6be311bb778d06cbf03c8e3
Victor Simonin <victor.simonin@epita.fr>
2021-12-16 18:14:22 +0100

    2021 day 6 exercise 2

--- 2021/6/6.py
+++ 2021/6/6.py
@@ -22,20 +22,18 @@ def lanternfish(lines):
     #print("Initial State:", fishes)
 
     while (now < days):
+        new_fish = []
         for i in range(len(fishes)):
             fishes[i] -= 1
 
-        new_fish = 0
-        for i in range(len(fishes)):
             if fishes[i] < 0:
+                new_fish.append(8)
                 fishes[i] = 6
-                new_fish += 1
 
-        for i in range(new_fish):
-            fishes.append(8)
+        fishes = fishes + new_fish
                 
         now += 1
-        #print("After", now, "days:", fishes)
+        #print("After", now, "days")
 
     return len(fishes)
 
@@ -46,4 +44,40 @@ print ("\nThe Result of the first test is :", result)
 lines = import_data()
 result = lanternfish(lines)
 print ("The Result of the first exercise is :",result)
+print()
+
+############### Second Exercise ###############    
+
+from collections import Counter
+
+def fast(lines):
+    fishes = lines[0].split(',')
+
+    for i in range(len(fishes)):
+        fishes[i] = int(fishes[i])
+
+    lifes = dict(Counter(fishes))
+
+    days = 256
+    for day in range(1, days+1):
+        lifes = {l: (0 if lifes.get(l+1) is None else lifes.get(l+1)) for l in range(-1, 8)}
+        
+        # make all 8s -1 because we create new fish with 8 after it reaches 0
+        lifes[8] = lifes[-1]
+        
+        # add new lifes to that are exhausted
+        lifes[6] += lifes[-1]
+        
+        # reset exhausted lifes
+        lifes[-1] = 0 
+
+    return sum(lifes.values())
+
+lines = import_test()
+result = fast(lines)
+print ("\nThe Result of the second test is :", result)
+
+lines = import_data()
+result = fast(lines)
+print ("The Result of the second exercise is :",result)
 print()
