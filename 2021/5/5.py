9ba4635ec6b2f2364cb7c07b3cd3ece93ee867e1
Victor Simonin <victor.simonin@epita.fr>
2021-12-16 16:59:25 +0100

    2021 day 5 exercise 2

--- 2021/5/5.py
+++ 2021/5/5.py
@@ -39,6 +39,7 @@ def get_coordinates(lines):
 
 
     return coordinates
+
 def overlap(lines):
     coordinates = get_coordinates(lines)
 
@@ -77,4 +78,77 @@ print ("\nThe Result of the first test is :", result)
 lines = import_data()
 result = overlap(lines)
 print ("The Result of the first exercise is :",result)
+print()
+
+############### Second Exercise ###############    
+
+def overlap_diag(lines):
+    coordinates = get_coordinates(lines)
+
+    map = [[0 for i in range(1000)] for j in range(1000)]
+    
+    for c in coordinates:
+        if abs(c.firstx - c.secondx) == abs(c.firsty - c.secondy):
+            if c.firstx < c.secondx:
+                if c.firsty < c.secondy:
+                    x = c.firstx
+                    y = c.firsty
+                    for i in range(c.firstx, c.secondx + 1):
+                        map[y][x] += 1
+                        x += 1
+                        y += 1
+                else:
+                    x = c.firstx
+                    y = c.firsty
+                    for i in range(c.firstx, c.secondx + 1):
+                        map[y][x] += 1
+                        x += 1
+                        y -= 1
+            else:
+                if c.firsty < c.secondy:
+                    x = c.firstx
+                    y = c.firsty
+                    for i in range(c.secondx, c.firstx + 1):
+                        map[y][x] += 1
+                        x -= 1
+                        y += 1
+                else:
+                    x = c.firstx
+                    y = c.firsty
+                    for i in range(c.secondx, c.firstx + 1):
+                        map[y][x] += 1
+                        x -= 1
+                        y -= 1
+        elif c.firstx == c.secondx:
+            if c.firsty < c.secondy:
+                for i in range(c.firsty, c.secondy + 1):
+                    map[i][c.firstx] += 1
+            else:
+                for i in range(c.secondy, c.firsty + 1):
+                    map[i][c.firstx] += 1
+        elif c.firsty == c.secondy:
+            if c.firstx < c.secondx:
+                for i in range(c.firstx, c.secondx + 1):
+                    map[c.firsty][i] += 1
+            else:
+                for i in range(c.secondx, c.firstx + 1):
+                    map[c.firsty][i] += 1
+
+    result = 0
+
+    for m in map:
+        for i in range(len(m)):
+            if m[i] > 1:
+                result += 1
+
+    return result
+
+lines = import_test()
+result = overlap_diag(lines)
+print ("\nThe Result of the second test is :", result)
+
+
+lines = import_data()
+result = overlap_diag(lines)
+print ("The Result of the second exercise is :",result)
 print()
